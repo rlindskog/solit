6dd9dc4c9fe58ad14a4c2613d5311f04a1735c5a
Ryan Lindskog <r.lindskog17@gmail.com>
2017-07-31 17:00:25 -0700

    Set app to listen after previous process is killed.

--- config/entry.js
+++ config/entry.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import app from 'srcdir'
 
-if (module.hot) module.hot.accept('srcdir')
-
-export default express()
-  .use((req, res) => app.handle(req, res))
-  .listen(process.env.PORT, process.env.HOST, err => {
-    if (err) {
-      console.error(err)
-      return
-    }
+setImmediate(() => {
+  if (module.hot) module.hot.accept('srcdir')
+  app.listen(process.env.PORT, process.env.HOST, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
     console.log(`🔥  Listening at http://${process.env.HOST}:${process.env.PORT}`)
   })
+})
